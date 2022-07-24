a70e2b38c06430fd687d9cc2c7dbd492f125a0c1
brandonsams <brandonkeithsams@gmail.com>
2022-07-24 13:42:20 -0600

    added some data storage code

--- crumbl.py
+++ crumbl.py
@@ -1,23 +1,59 @@
+from multiprocessing import set_forkserver_preload
 import requests
 from bs4 import BeautifulSoup as bs
 import shutil
+import datetime
+import pandas as pd
+import collections
 
 
-class Cookie:
+class CookieJar:
+    cookie_csv_filename = "cookies.csv"
+
+    def __init__(self, cookie_jar_dir="./cookie_jar"):
+        self.cookie_jar_dir = cookie_jar_dir
+        self.cookie_collection = []
+        # self.load_old_cookies()
+
+    def add_cookie(self, new_cookie):
+        self.cookie_collection.append(new_cookie)
+
+    def load_old_cookies(self):
+        df = pd.read_csv(
+            filepath=f"{self.cookie_jar_dir}/{CookieJar.cookie_csv_filename}", index_col="id")
+        old_cookies = [(Cookie(row.HourOfDay, row.Percentage))
+                       for index, row in df.iterrows()]
+
+    def save_to_cookie_jar_dir(self):
+        df = pd.DataFrame([c.__dict__ for c in self.cookie_collection])
+        df.to_csv(
+            f"{self.cookie_jar_dir}/{CookieJar.cookie_csv_filename}", index_label="id")
+
+
+class Cookie():
     def __init__(self, title, description, image_url):
         self.title = title
         self.description = description
         self.image_url = image_url
+        self.image_filename = image_url.split("/")[-1]
+        self.date = datetime.date.today()
 
     def save_cookie_image(self, dest_dir):
-        filename = self.image_url.split("/")[-1]
-        out_path = f"{dest_dir}/{filename}"
+        out_path = f"{dest_dir}/{self.image_filename}"
         # Get request on full_url
         r = requests.get(self.image_url, stream=True)
         if r.status_code == 200:  # 200 status code = OK
             with open(out_path, 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
+            
+    @classmethod
+    def from_df_row(self, df_row):
+        # TODO: Finish this part
+         Reading(row.HourOfDay,row.Percentage)) for index, row in df.iterrows() ]
+
+    # def __repr__(self):
+    #     return f'{self.__class__.__name__}> (title {self.title}, description {self.description}, image_url {self.image_url}, image_filename {self.image_filename}, date {self.date})'
 
     def __str__(self):
         return f"{self.title} | {self.description}"
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import crumbl
 
+cookie_jar = crumbl.CookieJar()
+
 cc = crumbl.CookieCrawler()
 cookies = cc.find_cookies()
 for cookie in cookies:
-    cookie.save_cookie_image("./img")
+    cookie.save_cookie_image("./cookie_jar/images")
+    cookie_jar.add_cookie(cookie)
+
+cookie_jar.save_to_cookie_jar_dir()
